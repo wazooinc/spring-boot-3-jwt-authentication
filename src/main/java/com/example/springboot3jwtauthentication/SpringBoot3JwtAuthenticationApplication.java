83b0d2b8d28082cb21a43d5468ae432ff616fcaf
erikyuzwa <erikyuzwa@gmail.com>
2023-07-30 07:55:56 -0600

    initial commit

---
+++ src/main/java/com/example/springboot3jwtauthentication/SpringBoot3JwtAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot3jwtauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3JwtAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot3JwtAuthenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot3jwtauthentication/SpringBoot3JwtAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot3jwtauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3JwtAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
